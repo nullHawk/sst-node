ee117fa5108757a64f1e70265943a628ceaa80e2
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-07-01 11:28:34 +0530

    added check for /login

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html><head><title>Node js class</title></head><body><h1>Hello World!</body></html>"); 
-    res.end();
+    if (req.url === '/login' && req.method === 'GET') {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html><head><title>Node js class</title></head><body><h1>Logged In</body></html>"); 
+        res.end();
+      } else {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html><head><title>Node js class</title></head><body><h1>Hello World!</body></html>"); 
+        res.end();
+      }
 });
 
 const port = 8000;
